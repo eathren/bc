338f487e2877d63f5a63a48e9a223f04fd410935
nolanbraman <nolan.braman@gmail.com>
2024-12-21 19:27:48 -0800

    linting, core app

--- bc/settings.py
+++ bc/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
