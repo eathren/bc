348fd2267c31870386cc4d8afbdb6412500e28ce
nolanbraman <nolan.braman@gmail.com>
2024-12-21 20:13:53 -0800

    fix bulma cdn, add cards?

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class BusinessCard(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} at {self.company_name}"
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import BusinessCard
 
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard/dashboard.html')
+    business_cards = BusinessCard.objects.filter(user=request.user)
+    return render(request, 'dashboard/dashboard.html', {'business_cards': business_cards})
