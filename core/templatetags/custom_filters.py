79257f4640934adcaaff0df39455c21231586318
eathren <nolan.braman@gmail.com>
2025-01-13 16:32:03 -0800

    custom auth message filters

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
+
+@register.filter
+def auth_messages(messages):
+    auth_tags = ['login', 'signup', 'password_reset', 'password_change']
+    return [message for message in messages if message.extra_tags in auth_tags]
